3193abd844bae92159258a5cc80221e9a7d16b06
Aaron Blasdel <ablasdel@willowgarage.com>
2013-03-07 14:45:54 -0800

    Remove bin copy of xacro.py

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['xacro'],
-    package_dir={'': 'src'},
-    scripts=['scripts/xacro.py']
+    package_dir={'': 'src'}
 )
 
 setup(**d)
