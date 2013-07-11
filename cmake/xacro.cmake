284d24f9884f15a420c6fc9ce81a53481997d644
JD Yamokoski <yamokosk@saint.(none)>
2013-07-11 17:12:16 -0500

    - Added error handling to execute_process - Fixed execute_process call to actually convert xacros to urdf

--- cmake/xacro.cmake
+++ cmake/xacro.cmake
@@ -3,15 +3,20 @@ set(_xacro_py ${xacro_PACKAGE_PATH}/xacro.py)
 
 macro(xacro_add_xacro_file input output)
   # Call out to xacro to get dependencies
-  execute_process(COMMAND ${_xacro_py} --deps ${input}
-    ERROR_VARIABLE _xacro_err_ignore
-    OUTPUT_VARIABLE _xacro_deps_result
+  execute_process(COMMAND ${_xacro_py} ${input}
+    ERROR_VARIABLE _xacro_err
+    OUTPUT_FILE ${output}
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  separate_arguments(_xacro_deps_result)
+  if (_xacro_err)
+    message(SEND_ERROR "Error occured while trying to execute xacro: ${_xacro_err}")
+  else (_xacro_err)
+    #message(STATUS " - Xacro processing results: ${_xacro_deps_result}")
+    separate_arguments(_xacro_deps_result)
 
-  add_custom_command(OUTPUT ${output}
-    COMMAND ${_xacro_py}
-    ARGS ${input} > ${output}
-    DEPENDS ${input} ${_xacro_deps_result})
+    add_custom_command(OUTPUT ${output}
+      COMMAND ${_xacro_py}
+      ARGS ${input} > ${output}
+      DEPENDS ${input} ${_xacro_deps_result})
+  endif (_xacro_err)
 endmacro(xacro_add_xacro_file)
