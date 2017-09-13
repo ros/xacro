4f65a2e128dfa4f10e968f45ca304dd24c764f7a
Kartik Mohta <kartikmohta@gmail.com>
2017-09-12 23:53:46 -0400

    Add xml.dom.minidom import

--- src/xacro/xmlutils.py
+++ src/xacro/xmlutils.py
@@ -30,7 +30,7 @@
 # Authors: Stuart Glaser, William Woodall, Robert Haschke
 # Maintainer: Morgan Quigley <morgan@osrfoundation.org>
 
-import xml
+import xml.dom.minidom
 from .color import warning
 
 def first_child_element(elt):
