70d970ac97528aa5e12d85a717c5b1345c4eb3ba
vandanamandlik <vandana_mandlik@persistent.com>
2018-10-24 17:03:01 +0530

    Modified setup.py to use python 3.5 version. Added data_files configuration settings. Modified entry_points setttings.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/python3.5
+
 from setuptools import find_packages
 from setuptools import setup
 
@@ -8,19 +9,21 @@ setup(
     name=package_name,
     version='1.13.4',
     packages=find_packages(exclude=['test']),
-    py_modules=['xacro'],
-    #data_files=[
-    #    ('share/ament_index/resource_index/packages',
-    #        ['resource/' + package_name]),
-    #    ('share/' + package_name, ['package.xml']),
-    #],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/'+ package_name, [
+            'resource/completion.bash'
+        ]),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     author='Robert Haschke',
     author_email='rhaschke@techfak.uni-bielefeld.de',
     maintainer='Robert Haschke',
     maintainer_email='rhaschke@techfak.uni-bielefeld.de',
-    keywords=['ROS2'],
+    keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
@@ -28,11 +31,14 @@ setup(
         'Topic :: Software Development',
     ],
     description='Xacro is an XML macro language. With xacro, you can construct shorter and more readable XML files by using macros that expand to larger XML expressions.',
-    license='',
-    test_suite='test',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
     entry_points={
+        'xacro.xacro': [
+            'cli = xacro.xacro.cli:ColoredOptionParser',
+        ],
         'console_scripts': [
-            'xacro = xacro:main'
+            'xacro = resource.xacro:main'
         ],
     },
 )
