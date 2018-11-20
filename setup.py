10b786725f5f2a73007059038cd8381b12fbff04
vandanamandlik <vandana_mandlik@persistent.com>
2018-11-20 14:30:00 +0530

    Modified package structure as per ROS2 rules
    - Removed CMakeLists.txt
    - Removed wrapper scripts: xacro.py and scripts/xacro
    - Moved source contents from "src/xacro" to "xacro"
    - Modifed package.xml and setup.py according to ROS2 requirements
    - Added linters: test_copyright, test_flake8, test_pep257
    - Configure completion hook

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
-#!/usr/bin/env python
+from setuptools import find_packages
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'xacro'
 
-d = generate_distutils_setup(
-    packages=['xacro'],
-    package_dir={'': 'src'},
-    scripts=['scripts/xacro']
+setup(
+    name=package_name,
+    python_requires='>=3',
+    packages=find_packages(),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/environment', ['scripts/completion.bash']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Robert Haschke',
+    maintainer_email='rhaschke@techfak.uni-bielefeld.de',
+    url='https://github.com/ros/xacro',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Xacro is an XML macro language.\n'
+                'With xacro, you can construct shorter and more readable XML files '
+                'by using macros that expand to larger XML expressions.',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'xacro = xacro:main'
+        ],
+    },
 )
-
-setup(**d)
