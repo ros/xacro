434e20533ffddfee9901e1c987fba79c7781f8fd
vandanamandlik <vandana_mandlik@persistent.com>
2018-11-20 14:30:00 +0530

    Modifed package structure as per ROS2 rules - Moved contents from "src/xacro/" folder to "xacro/" folder. - Renamed scripts folder to resource folder. - Modifed package.xml and setup.py according to ROS2 rules. - Deleted CMakeLists.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import find_packages
+from setuptools import setup
 
-d = generate_distutils_setup(
-    packages=['xacro'],
-    package_dir={'': 'src'},
-    scripts=['scripts/xacro'],
-    requires=['xml', 'roslaunch', 'rosgraph']
+package_name = 'xacro'
+
+setup(
+    name=package_name,
+    version='1.13.4',
+    python_requires='>=3',
+    packages=find_packages(exclude=['test']),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Robert Haschke',
+    author_email='rhaschke@techfak.uni-bielefeld.de',
+    maintainer='Robert Haschke',
+    maintainer_email='rhaschke@techfak.uni-bielefeld.de',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Xacro is an XML macro language. With xacro,'
+    'you can construct shorter and more readable XML files'
+    'by using macros that expand to larger XML expressions.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    test_suite='test',
+    entry_points={
+        'console_scripts': [
+            'xacro = resource.xacro:main'
+        ],
+    },
 )
-
-setup(**d)
