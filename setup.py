615c1ee45aba8068e17765c0f0c5a59515024841
David V. Lu <davidvlu@gmail.com>
2020-05-20 17:19:47 -0400

    Fixes #237

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['xacro'],
     package_dir={'': 'src'},
-    scripts=['scripts/xacro']
 )
 
 setup(**d)
