b95ee8afa16b8921bb5a7f8b3ebf22789b372330
Dustin Gooding <dgooding@houstonmechatronics.com>
2020-08-08 17:33:15 -0500

    reverting d1171d06d0ebef15eac246ca1b6ef24c3314c9c1

--- src/xacro/__init__.py
+++ src/xacro/__init__.py
@@ -94,20 +94,6 @@ def abs_filename_spec(filename_spec):
     return filename_spec
 
 
-class YamlDictWrapper(object):
-    """Wrapper class providing dotted access to dict items"""
-    def __init__(self, d):
-        self.__d = d
-
-    def __getattr__(self, item):
-        try:
-            result = self.__d.__getitem__(item)
-            return YamlDictWrapper(result) if isinstance(result, dict) else result
-        except KeyError:
-            raise XacroException("No such key: '{}'".format(item))
-
-    __getitem__ = __getattr__
-
 def load_yaml(filename):
     try:
         import yaml
@@ -118,7 +104,7 @@ def load_yaml(filename):
     f = open(filename)
     oldstack = push_file(filename)
     try:
-        return YamlDictWrapper(yaml.safe_load(f))
+        return yaml.safe_load(f)
     finally:
         f.close()
         restore_filestack(oldstack)
