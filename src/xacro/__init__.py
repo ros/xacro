205c30d2bd6094a54e801bd456e84fdb062aed4e
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-03-31 10:17:11 +0200

    Use scoped macro namespace for macro evaluation

--- src/xacro/__init__.py
+++ src/xacro/__init__.py
@@ -695,6 +695,7 @@ def handle_macro_call(node, macros, symbols):
 
     # Expand the macro
     scoped = Table(symbols)  # new local name space for macro evaluation
+    scoped_macros = Table(macros)
     params = m.params[:]  # deep copy macro's params list
     for name, value in node.attributes.items():
         if name not in params:
@@ -735,7 +736,7 @@ def handle_macro_call(node, macros, symbols):
         raise XacroException("Undefined parameters [%s]" % ",".join(params), macro=m)
 
     try:
-        eval_all(body, macros, scoped)
+        eval_all(body, scoped_macros, scoped)
     except Exception as e:
         # fill in macro call history for nice error reporting
         if hasattr(e, 'macros'):
@@ -942,7 +943,7 @@ def process_doc(doc, mappings=None, **kwargs):
     if not filestack:
         restore_filestack([None])
 
-    macros = {}
+    macros = Table()
     symbols = Table(global_symbols)
 
     # apply xacro:targetNamespace as global xmlns (if defined)
