49f9f1d7c6cf91650700bb5239c78c8c3ceab22a
Alec Tiefenthal <alectief@gmail.com>
2023-05-18 10:11:57 -0400

    Updated error type in YamlDictWrapper
    This allows the `hasattr` function to work properly.
    
    Also changed the error message to be more similar to the standard error message for attribute access.

--- src/xacro/__init__.py
+++ src/xacro/__init__.py
@@ -110,7 +110,7 @@ class YamlDictWrapper(dict):
         try:
             return YamlListWrapper.wrap(super(YamlDictWrapper, self).__getitem__(item))
         except KeyError:
-            raise XacroException("No such key: '{}'".format(item))
+            raise AttributeError("'YamlDictWrapper' object has no attribute '{}'".format(item))
 
     __getitem__ = __getattr__
 
